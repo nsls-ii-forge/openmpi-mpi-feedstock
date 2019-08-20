1d119ef537014e2b04660e0e5d5a81397178f8a7
buijennifer <bui.jennifert@gmail.com>
2019-08-20 20:28:09 +0000

    files for initial render

---
+++ recipe/build-mpi.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# unset unused old fortran compiler vars
+unset F90 F77
+
+export FCFLAGS="$FFLAGS"
+
+# avoid absolute-paths in compilers
+export CC=$(basename "$CC")
+export CXX=$(basename "$CXX")
+export FC=$(basename "$FC")
+
+if [ $(uname) == Darwin ]; then
+    if [[ ! -z "$CONDA_BUILD_SYSROOT" ]]; then
+        export CFLAGS="$CFLAGS -isysroot $CONDA_BUILD_SYSROOT"
+        export CXXFLAGS="$CXXFLAGS -isysroot $CONDA_BUILD_SYSROOT"
+    fi
+    export LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib"
+fi
+
+export LIBRARY_PATH="$PREFIX/lib"
+
+./configure --prefix=$PREFIX \
+            --disable-dependency-tracking \
+            --enable-mpi-cxx \
+            --enable-mpi-fortran \
+            --disable-wrapper-rpath \
+            --disable-wrapper-runpath \
+            --with-wrapper-cflags="-I$PREFIX/include" \
+            --with-wrapper-cxxflags="-I$PREFIX/include" \
+            --with-wrapper-fcflags="-I$PREFIX/include" \
+            --with-wrapper-ldflags="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib" \
+            --with-sge
+
+make -j"${CPU_COUNT:-1}"
+make install
